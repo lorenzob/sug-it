af5456e0a63ff66071e6eaefeffce6cf4426390e
lorenzob <trz@samarcanda.(none)>
2009-05-20 19:59:09 +0200

    mainizzato cosi' gira da eclipse, implementato list

--- prisma/prisma.scala
+++ prisma/prisma.scala
@@ -1,15 +1,31 @@
-def follow(x: String)= println("follow "+x)
-def show (x: String)= println("show "+x)
-def list() = println("list")
+import scala.io.Source
+import java.io.File
+  
+object prisma {
+  
+	def follow(x: String)= println("follow "+x)
+	
+	def list()  {
+	  val f = new File(".prisma")
+	  if (f.exists)
+		Source.fromFile(f).getLines.foreach(print)
+	}
+ 
+	def show(x: String) = println("list")
+	
+	def usage() = { 
+	    println("usage:\n [follow <word>|show <word>|list]")
+	    exit(1)
+	}
 
-def usage() = { 
-    println("usage:\n [follow <word>|show <word>|list]")
-    exit(1)
-}
-args match {
-    case Array("follow", word) => follow(word)
-    case Array("show", word)   => show(word)
-    case Array("list")         => list()
-    case _ => usage()
+	def main(args: Array[String]) {
+	  
+		args match {
+		    case Array("follow", word) => follow(word)
+		    case Array("show", word)   => show(word)
+		    case Array("list")         => list()
+		    case _ => usage()
+		}
+	}
 }
 // vim: set ts=4 sw=4 et:
